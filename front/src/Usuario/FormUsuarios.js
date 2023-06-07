4102043a1c8f3b9e525ae892cd7c539d4c6086e4
Carlos Gomez <carlosandres0741@gmail.com>
2023-06-06 23:23:53 -0500

    Correccion formulario de creacion

--- front/src/Usuario/FormUsuarios.js
+++ front/src/Usuario/FormUsuarios.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import { agregarUsuario } from '../Api/UsuariosApi';
 //import Navegador from '../Components/Navegador'
 
-function FormUsuario(props) {
-  console.log("Hola",props)
-const {getUser, setUser, otro, registrar=false} = props;
+function FormUsuario() {
 
 const [usuario,setUsuario]= useState({
             id_: null,
@@ -21,20 +20,22 @@ const [usuario,setUsuario]= useState({
 })
 const handleChange=(e)=>{
     setUsuario({
-        ...usuario, [e.target.Nombre] : e.target.value
+        ...usuario, [e.target.name] : e.target.value
     })
 }
 
-useEffect(() =>{
+const handleSubmit = async e => {
+  e.preventDefault()
+
+  const response = await agregarUsuario(usuario)
+
+  console.log(response)
+}
+
+/* useEffect(() =>{
   if(setUser)
     setUsuario(setUser)
-}, [setUser])
-
-const onclickGuardar = (e) => {
-  e.preventDefault();
-  otro(usuario);
-  //limpiar();
-};
+}, [setUser]) */
 
   return (
       <div>
@@ -45,47 +46,87 @@ const onclickGuardar = (e) => {
                       <h5 className="mb-0">Agregar Usuario</h5>
                     </div>
                     <div className="card-body">
-                      <form>
+                      <form onSubmit={handleSubmit}>
                         <div className="row mb-3">
                           <label className="col-sm-2 col-form-label">Nombre</label>
                           <div className="col-sm-10">
-                            <input type="text" className="form-control" id="nomUsuario" name="nombre" placeholder="Digite el nombre del Usuario" onChange={(e)=>{handleChange(e)}} />
+                            <input 
+                              type="text"
+                              className="form-control"
+                              id="nomUsuario"
+                              name="nombre"
+                              placeholder="Digite el nombre del Usuario"
+                              onChange={handleChange} />
                           </div>
                         </div>
                         <div className="row mb-3">
                           <label className="col-sm-2 col-form-label">Apellidos</label>
                           <div className="col-sm-10">
-                            <input type="text" className="form-control" id="apellido" name="apellido" placeholder="Digite el apellido del Usuario" onChange={(e)=>{handleChange(e)}} />
+                            <input
+                             type="text"
+                              className="form-control"
+                              id="apellido"
+                              name="apellido"
+                              placeholder="Digite el apellido del Usuario"
+                              onChange={handleChange} />
                           </div>
                         </div>
                         <div className="row mb-3">
                           <label className="col-sm-2 col-form-label">Fecha de nacimiento</label>
                           <div className="col-sm-10">
-                            <input type="date" className="form-control" id="fecha_nacimiento" name="fecha_nacimiento" placeholder="Digite la fecha de nacimiento del Usuario" onChange={(e)=>{handleChange(e)}} />
+                            <input 
+                              type="date"
+                              className="form-control" 
+                              id="fecha_nacimiento" 
+                              name="fecha_nacimiento" 
+                              placeholder="Digite la fecha de nacimiento del Usuario" 
+                              onChange={handleChange} />
                           </div>
                         </div>
                         <div className="row mb-3">
                           <label className="col-sm-2 col-form-label">Dirección</label>
                           <div className="col-sm-10">
-                            <input type="text" className="form-control" id="direccion" name="direccion" placeholder="Digite la dirección del Usuario" onChange={(e)=>{handleChange(e)}} />
+                            <input 
+                              type="text" 
+                              className="form-control" 
+                              id="direccion" 
+                              name="direccion" 
+                              placeholder="Digite la dirección del Usuario" 
+                              onChange={handleChange} />
                           </div>
                         </div>
                         <div className="row mb-3">
                           <label className="col-sm-2 col-form-label">Correo</label>
                           <div className="col-sm-10">
-                            <input type="text" className="form-control" id="correo" name="correo" placeholder="Digite el correo del Usuario" onChange={(e)=>{handleChange(e)}} />
+                            <input 
+                              type="text" 
+                              className="form-control" 
+                              id="correo" 
+                              name="correo" 
+                              placeholder="Digite el correo del Usuario" 
+                              onChange={handleChange} />
                           </div>
                         </div>
                         <div className="row mb-3">
                           <label className="col-sm-2 col-form-label">Telefono</label>
                           <div className="col-sm-10">
-                            <input type="text" className="form-control" id="telefono" name="telefono" placeholder="Digite el telefono del Usuario" onChange={(e)=>{handleChange(e)}} />
+                            <input 
+                              type="text" 
+                              className="form-control" 
+                              id="telefono" 
+                              name="telefono" 
+                              placeholder="Digite el telefono del Usuario" 
+                              onChange={handleChange} />
                           </div>
                         </div>
                         <div className="row mb-3">
                         <label className="col-sm-2 col-form-label">Genero</label>
                         <div className="col-sm-10">
-                        <select className="form-select" id="genero" name="genero" onChange={(e)=>{handleChange(e)}}>
+                        <select 
+                          className="form-select" 
+                          id="genero" 
+                          name="genero" 
+                          onChange={handleChange}>
                           <option selected="">Seleccionar Opción</option>
                           <option value="Femenino">Femenino</option>
                           <option value="Masculino">Masculino</option>
@@ -95,13 +136,23 @@ const onclickGuardar = (e) => {
                       <div className="row mb-3">
                           <label className="col-sm-2 col-form-label">Contraseña</label>
                           <div className="col-sm-10">
-                            <input type="password" className="form-control" id="contrasena" name="contrasena" placeholder="Digite la contraseña del Usuario" onChange={(e)=>{handleChange(e)}} />
+                            <input 
+                              type="password" 
+                              className="form-control" 
+                              id="contrasena" 
+                              name="contrasena" 
+                              placeholder="Digite la contraseña del Usuario" 
+                              onChange={handleChange} />
                           </div>
                         </div>
                         <div className="row mb-3">
                         <label className="col-sm-2 col-form-label">Rol</label>
                         <div className="col-sm-10">
-                        <select className="form-select" id="rol" name="rol" onChange={(e)=>{handleChange(e)}}>
+                        <select 
+                          className="form-select" 
+                          id="rol" 
+                          name="rol" 
+                          onChange={handleChange}>
                           <option selected="">Seleccionar Opción</option>
                           <option value="administrador">Administrador</option>
                           <option value="personal">Personal</option>
@@ -112,7 +163,11 @@ const onclickGuardar = (e) => {
                         <div className="row mb-3">
                         <label className="col-sm-2 col-form-label">Estado</label>
                         <div className="col-sm-10">
-                        <select className="form-select" id="estado" name="estado" onChange={(e)=>{handleChange(e)}}>
+                        <select 
+                          className="form-select" 
+                          id="estado" 
+                          name="estado" 
+                          onChange={handleChange}>
                           <option selected="">Seleccionar Opción</option>
                           <option value="1">Activo</option>
                           <option value="0">Inactivo</option>
@@ -121,7 +176,7 @@ const onclickGuardar = (e) => {
                       </div>
                         <div className="row justify-content-end">
                           <div className="col-sm-10">
-                            <button type="button" className="btn btn-primary" onClick={onclickGuardar}>Guardar</button>
+                            <button type="submit" className="btn btn-primary">Guardar</button>
                             <a href="#" className="btn btn-danger">Cancelar</a>
                           </div>
                           </div>
